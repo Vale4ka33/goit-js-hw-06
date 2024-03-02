0898fe50ca08e72e15e9c078e4124cd51daf01df
Vale4ka33 <150661954+Vale4ka33@users.noreply.github.com>
2024-03-02 23:47:37 +0200

    hw-6 js done

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,24 @@
-class StringBuilder
+class StringBuilder {
+    #value;
+
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
+    getValue() {
+        return this.#value;
+    }
+    padEnd(str) {
+         this.#value += str;
+    }
+    padStart(str) {
+        this.#value = str + this.#value;
+
+    }
+    padBoth(str) {
+        this.#value = str + this.#value +str;
+
+    }
+}
 
 
 const builder = new StringBuilder(".");
